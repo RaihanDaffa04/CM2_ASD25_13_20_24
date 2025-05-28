41d81adffbc687c48790b92beb3ea376aaddf2f9
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2025-05-28 09:43:53 +0700

    commit menu 3

--- src/SistemAntrian.java
+++ src/SistemAntrian.java
@@ -1,3 +1,38 @@
 public class SistemAntrian {
-    NodePasien head=null;
+    NodePasien head = null;
+    NodeDokter headDokter = null;
+
+    
+    public SistemAntrian() {
+        tambahDokter(new Dokter("dr01", "Wike Ratanca"));
+        tambahDokter(new Dokter("dr02", "Santi Runica"));
+        tambahDokter(new Dokter("dr03", "Aam Antanica"));
+        tambahDokter(new Dokter("dr04", "Slamet Sugito"));
+    }
+
+    void tambahDokter(Dokter d) {
+        NodeDokter baru = new NodeDokter(d);
+        if (headDokter == null) {
+            headDokter = baru;
+        } else {
+            NodeDokter current = headDokter;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = baru;
+        }
+    }
+
+    Dokter cariDokter(String kode) {
+        NodeDokter current = headDokter;
+        while (current != null) {
+            if (current.data.id.equalsIgnoreCase(kode)) {
+                return current.data;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    
 }
