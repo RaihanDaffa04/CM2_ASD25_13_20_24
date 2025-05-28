c37789be95c308b2520f2c963cb6c96428408437
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2025-05-28 10:53:03 +0700

    commit main menu 3 dan 4

--- src/SistemAntrian.java
+++ src/SistemAntrian.java
@@ -5,38 +5,37 @@ public class SistemAntrian {
     NodeDokter headDokter = null;
     NodeTransaksi headTransaksi, tailTransaksi;
     int size;
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
 
     public void tambahPasien(Pasien pasien) {
         NodePasien newNode = new NodePasien(headPasien, pasien, null);
-            if (headPasien == null) {  
-                headPasien = newNode;  
-            } else {
-                NodePasien temp = headPasien;
-                while (temp.next != null) {  
-                    temp = temp.next;
-                }
-                temp.next = newNode; 
-            size++; 
+        if (headPasien == null) {
+            headPasien = tailPasien = newNode;
+        } else {
+            tailPasien.next = newNode;
+            newNode.prev = tailPasien;
+            tailPasien = newNode;
         }
+        size++;
     }
 
     public void lihatAntrian() {
-        if (headPasien == null) {
+        if (isEmpty()) {
             System.out.println("Tidak ada pasien dalam antrian.");
         } else {
             NodePasien temp = headPasien;
             System.out.println("-- Antrian Pasien --");
             while (temp != null) {
-                System.out.println("Nama: " + temp.data.nama + " | NIK: " + temp.data.nik + " | Keluhan: " + temp.data.keluhan);
-                temp = temp.next;  
+                System.out.println(
+                        "Nama: " + temp.data.nama + " | NIK: " + temp.data.nik + " | Keluhan: " + temp.data.keluhan);
+                temp = temp.next;
             }
         }
     }
-    
+
     public SistemAntrian() {
         tambahDokter(new Dokter("dr01", "Wike Ratanca"));
         tambahDokter(new Dokter("dr02", "Santi Runica"));
@@ -58,60 +57,65 @@ public class SistemAntrian {
     }
 
     public Pasien layaniPasien() throws Exception {
-    if (headPasien == null) {
-        throw new Exception("Antrian kosong, tidak ada pasien yang bisa dilayani!");
-    }
-
-    Pasien p = headPasien.data;
-    if (headPasien == tailPasien) {
-        headPasien = tailPasien = null;
-    } else {
-        headPasien = headPasien.next;
-        headPasien.prev = null;
-    }
-    size--;
-
-    System.out.println("Pasien " + p.nama + " dipanggil");
-    System.out.println("Daftar dokter jaga :");
-    System.out.println("Kode\tNama");
-    NodeDokter current = headDokter;
-    while (current != null) {
-        System.out.println(current.data.id + "\t" + current.data.nama);
-        current = current.next;
-    }
-    Scanner sc = new Scanner(System.in);
-    System.out.print("\ninput kode dokter: ");
-    String kode = sc.nextLine();
-    Dokter dipilih = null;
-    current = headDokter;
-    while (current != null) {
-        if (current.data.id.equalsIgnoreCase(kode)) {
-            dipilih = current.data;
-            break;
+        if (headPasien == null) {
+            throw new Exception("Antrian kosong, tidak ada pasien yang bisa dilayani!");
         }
-        current = current.next;
+
+        Pasien p = headPasien.data;
+        if (headPasien == tailPasien) {
+            headPasien = tailPasien = null;
+        } else {
+            headPasien = headPasien.next;
+            headPasien.prev = null;
+        }
+        size--;
+
+        System.out.println("Pasien " + p.nama + " dipanggil");
+        System.out.println("Daftar dokter jaga :");
+        System.out.println("Kode\tNama");
+        NodeDokter current = headDokter;
+        while (current != null) {
+            System.out.println(current.data.id + "\t" + current.data.nama);
+            current = current.next;
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\ninput kode dokter: ");
+        String kode = sc.nextLine();
+        Dokter dipilih = null;
+        current = headDokter;
+        while (current != null) {
+            if (current.data.id.equalsIgnoreCase(kode)) {
+                dipilih = current.data;
+                break;
+            }
+            current = current.next;
+        }
+
+        if (dipilih == null) {
+            System.out.println("Dokter tidak ditemukan.");
+            return null;
+        }
+
+        System.out.print("input durasi layanan(jam): ");
+        int durasi = sc.nextInt();
+
+        TransaksiLayanan trx = new TransaksiLayanan(p, dipilih, durasi);
+
+        NodeTransaksi newTrx = new NodeTransaksi(trx);
+
+        if (headTransaksi == null) {
+            headTransaksi = tailTransaksi = newTrx;
+        } else {
+            tailTransaksi.next = newTrx;
+            tailTransaksi = newTrx;
+        }
+
+        System.out.println(">> Pasien telah dilayani, transaksi berhasil dicatat");
+        return p;
+
     }
 
-    if (dipilih == null) {
-        System.out.println("Dokter tidak ditemukan.");
-        return null;
+    public int sisaAntrian() {
+        return size;
     }
-
-    System.out.print("input durasi layanan(jam): ");
-    int durasi = sc.nextInt();
-
-    TransaksiLayanan trx = new TransaksiLayanan(p, dipilih, durasi);
-
-    NodeTransaksi newTrx = new NodeTransaksi(trx);
-
-    if (headTransaksi == null) {
-        headTransaksi = tailTransaksi = newTrx;
-    } else {
-        tailTransaksi.next = newTrx;
-        tailTransaksi = newTrx;
-    }
-
-    System.out.println(">> Pasien telah dilayani, transaksi berhasil dicatat");
-    return p;
-}
 }
