0e427e7b7cfb398c613200720b44fc9c9cbb589f
daebon07 <muhammadshabran25@gmail.com>
2025-05-28 09:41:35 +0700

    commit method menu 1

--- src/SistemAntrian.java
+++ src/SistemAntrian.java
@@ -1,3 +1,23 @@
 public class SistemAntrian {
+    NodePasien headPasien=null;
+    int size;
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
 
+    public void tambahPasien(Pasien pasien) {
+        NodePasien newNode = new NodePasien(headPasien, pasien, null); 
+        if (headPasien == null) {  
+            headPasien = newNode;  
+        } else {
+            NodePasien temp = headPasien;
+            while (temp.next != null) {  
+                temp = temp.next;
+            }
+            temp.next = newNode; 
+        size++; 
+    }
+
+}
 }
