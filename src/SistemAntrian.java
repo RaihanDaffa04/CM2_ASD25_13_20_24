83a47dcfc1fe39e430fc22631574453b0ebb25bd
kmilaz14 <kamilazhwa@gmail.com>
2025-05-28 10:19:37 +0700

    menu 5 dan 6

--- src/SistemAntrian.java
+++ src/SistemAntrian.java
@@ -1,5 +1,6 @@
 public class SistemAntrian {
     NodePasien headPasien=null;
+    NodeTransaksi headTransaksi=null;
     int size;
     
     public boolean isEmpty(){
@@ -31,6 +32,36 @@ public class SistemAntrian {
                 temp = temp.next;  
             }
         }
+    }
+
+    public void tampilkanRiwayat() {
+        NodeTransaksi current = headTransaksi;
+        while (current != null) {
+            current.data.tampilkanTransaksi(); 
+        }
+    }
+
+    public void sortRiwayatDESC(){
+        if (headTransaksi==null || headTransaksi.next==null) {
+            return;
+        }
+        boolean tukar;
+        do {
+            tukar=false;
+            NodeTransaksi current = headTransaksi;
+
+            while (current.next != null) {
+                String nama1 = current.data.pasien.nama.toLowerCase();
+                String nama2 = current.next.data.pasien.nama.toLowerCase();
+                if (nama1.compareTo(nama2) < 0) {
+                    TransaksiLayanan temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    tukar = true;
+                }
+                current = current.next;
+            }
+        } while (tukar);
     }
     
 }
